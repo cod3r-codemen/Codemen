7afff64a9839e2b5925c7acf8620597ffe699fff
cod3r-codemen <cod3r@outlook.com.br>
2015-09-30 10:53:47 -0300

    update 1.0

--- index.php
+++ index.php
@@ -1,7 +1,225 @@
-<?php
-/**
- * Created by Cod3r Kane - J�lio C�sar Lopes.
- * Powered by www.codemen.com.br
- * Date: 30/09/2015
- * Time: 10:44
- */
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="Codemen - Seu Futuro na web. Somos uma pequena Startup, criada por amantes de tecnologias, programação e design. Nossa missão é desenvolver, simples assim.">
+        <meta name="author" content="Powered by Cod3r Kane - Júlio César Lopes - Codemen.">
+
+        <title>Codemen - Seu Futuro na Web</title>
+
+        <link rel="stylesheet" type="text/css" href="dist/semantic.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/reset.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/site.css">
+
+        <link rel="stylesheet" type="text/css" href="dist/components/container.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/grid.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/header.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/image.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/menu.css">
+
+        <link rel="stylesheet" type="text/css" href="dist/components/divider.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/dropdown.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/segment.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/button.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/list.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/icon.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/sidebar.css">
+        <link rel="stylesheet" type="text/css" href="dist/components/transition.css">
+        <link rel="stylesheet" type="text/css" href="_css/style.css">
+
+        <script src="_js/jquery.min.js"></script>
+        <script src="dist/components/visibility.js"></script>
+        <script src="dist/components/sidebar.js"></script>
+        <script src="dist/components/transition.js"></script>
+        <script src="dist/semantic.min.js"></script>
+        <script src="_js/main.js"></script>
+    </head>
+<body>
+
+<div class="ui large top fixed hidden menu">
+    <div class="ui container">
+        <a class="active item home">Home</a>
+        <a class="item portfolio">Portfólio</a>
+        <div class="right menu">
+            <div class="item">
+                <a class="ui button">Contato</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="ui vertical inverted sidebar menu">
+    <a class="active item">Home</a>
+    <a class="item portfolio">Portfólio</a>
+    <a class="item">Contato</a>
+</div>
+
+<div class="pusher">
+    <div class="ui inverted vertical masthead center aligned segment">
+
+        <div class="ui container">
+            <div class="ui large secondary inverted pointing menu">
+                <a class="toc item">
+                    <i class="sidebar icon"></i>
+                </a>
+                <a class="active item home">Home</a>
+                <a class="item portfolio">Portfólio</a>
+                <div class="right item">
+                    <a class="ui inverted button">Contato</a>
+                </div>
+            </div>
+        </div>
+        <div id="home" class="ui text container">
+            <h1 class="ui inverted header">
+                Codemen
+            </h1>
+            <h2>
+                Seu Futuro na Web.
+            </h2>
+            <p>
+                Seu site profissional, pessoal ou loja virtual em qualquer lugar em qualquer gadgets com web responsive.
+            </p>
+            <div id="budget" class="ui huge primary button">Solicitar Orçamento <i class="right arrow icon"></i></div>
+        </div>
+        <div id="portfolio" class="ui row text">
+            <div class="ui container text">
+                <h1 class="ui inverted header">
+                    Portfólio
+                </h1>
+                <p>
+                    Veja nossos trabalhos e inspire-se você também. Venha para o lado Codemen.
+                </p>
+            </div>
+            <div id="portfolioContent" class="ui container">
+                <div class="ui large secondary">
+                    <div class="ui four column padded grid">
+                        <div class="column">
+                            <div class="ui link cards">
+                                <div class="card">
+                                    <div class="image">
+                                        <img src="http://semantic-ui.com/images/avatar2/large/matthew.png">
+                                    </div>
+                                    <div class="content">
+                                        <div class="header">Developer System</div>
+                                        <div class="description">
+                                            Tell me, do you bleed?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="ui link cards">
+                                <div class="card">
+                                    <div class="image">
+                                        <img src="http://semantic-ui.com/images/avatar2/large/matthew.png">
+                                    </div>
+                                    <div class="content">
+                                        <div class="header">Developer System</div>
+                                        <div class="description">
+                                            Tell me, do you bleed?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="ui link cards">
+                                <div class="card">
+                                    <div class="image">
+                                        <img src="http://semantic-ui.com/images/avatar2/large/matthew.png">
+                                    </div>
+                                    <div class="content">
+                                        <div class="header">Developer System</div>
+                                        <div class="description">
+                                            Tell me, do you bleed?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="ui link cards">
+                                <div class="card">
+                                    <div class="image">
+                                        <img src="http://semantic-ui.com/images/avatar2/large/matthew.png">
+                                    </div>
+                                    <div class="content">
+                                        <div class="header">Developer System</div>
+                                        <div class="description">
+                                            Tell me, do you bleed?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="ui link cards">
+                                <div class="card">
+                                    <div class="image">
+                                        <img src="http://semantic-ui.com/images/avatar2/large/matthew.png">
+                                    </div>
+                                    <div class="content">
+                                        <div class="header">Developer System</div>
+                                        <div class="description">
+                                            Tell me, do you bleed?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="contact" class="ui text container">
+            <h1 class="ui inverted header">
+                Contato
+            </h1>
+        </div>
+    </div>
+</div>
+
+<div id="budgetModal" class="ui modal">
+    <i class="close icon"></i>
+    <div class="header">
+        Formulário de Orçamento
+    </div>
+    <div class="content">
+        <form method="GET" action="javascript:;" class="ui form">
+            <div class="field">
+                <label>
+                    Nome:
+                    <input type="text" name="first-name" placeholder="Nome" required>
+                </label>
+            </div>
+            <div class="field">
+                <label>
+                    E-mail:
+                    <input type="email" name="email" placeholder="E-mail" required>
+                </label>
+            </div>
+            <div class="field">
+                <label>
+                    Telefone:
+                    <input type="number" name="phone" placeholder="Telefone" required>
+                </label>
+            </div>
+            <div class="field">
+                <label>
+                    Mensagem:
+                    <textarea name="message" rows="4" placeholder="Conte-nos um pouco sobre suas ideias." required></textarea>
+                </label>
+            </div>
+        </form>
+    </div>
+    <div class="actions">
+        <div class="ui negative button">Fechar</div>
+        <button type="submit" class="ui positive button">Enviar</button>
+    </div>
+</div>
+
+</body>
+</html>
